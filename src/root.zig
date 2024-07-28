84c22afd1e67f647d1844144b42930445c4cc67c
Waine Oliveira Jr <waine@aerosim.io>
2024-07-27 23:22:36 -0300

    changed root to export all symbols automatically

--- src/root.zig
+++ src/root.zig
@@ -1,14 +1,9 @@
 const std = @import("std");
 const testing = std.testing;
-const queue = @import("queue.zig");
-const zz = @import("zigzagos_core.zig");
-
-export const z_queue_size = queue.queue_size;
-export const z_queue_print = queue.queue_print;
-export const z_queue_append = queue.queue_append;
-export const z_queue_remove = queue.queue_remove;
-
-export const z_ppos_init = zz.ppos_init;
+comptime {
+    _ = @import("queue.zig");
+    _ = @import("zigzagos_core.zig");
+}
 
 export fn add(a: i32, b: i32) i32 {
     return a + b;
